df2e04a38445e83f9637889905313f63bb7bd066
Wacil23 <wacilzek00@gmail.com>
2022-02-08 13:02:16 +0100

    Fait

--- variable2.php
+++ variable2.php
@@ -1,1 +1,9 @@
 <?php
+    $message1 = "0@sn9sirppa@#?ia’jgtvryko1";
+    $message2 = "q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
+    $message3 = "aopi?sgnirts@#?sedhtg+p9l!";
+
+    $lenght = mb_strlen($message1 / 2);
+    $substract = mb_substr($message1, 5, $lenght);
+    $replace = str_replace('@#?', ' ', $substract);
+    $reverse = strrev($replace);
