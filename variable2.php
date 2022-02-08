e82e88c5dff3278f31d2f6a4f8798be877b8b782
Wacil23 <wacilzek00@gmail.com>
2022-02-08 13:05:01 +0100

    Finish

--- variable2.php
+++ variable2.php
@@ -7,3 +7,15 @@
     $substract = mb_substr($message1, 5, $lenght);
     $replace = str_replace('@#?', ' ', $substract);
     $reverse = strrev($replace);
+
+    $lenght = mb_strlen($message2 / 2);
+    $substract = mb_substr($message2, 5, $lenght);
+    $replace = str_replace('@#?', ' ', $substract);
+    $reverse2 = strrev($replace);
+
+    $lenght = mb_strlen($message3 / 2);
+    $substract = mb_substr($message3, 5, $lenght);
+    $replace = str_replace('@#?', ' ', $substract);
+    $reverse3 = strrev($replace);
+
+    echo ($reverse . ' ' . $reverse2 . ' ' . $reverse3);
